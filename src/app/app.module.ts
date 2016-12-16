ebfe1a14abdcb688518c21166bfeeb3784fc834d
Seth Alt <seth@sweepingdesign.com>
2016-12-16 15:05:44 -0600

    stubbed in html from approved round 2 mockups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,20 @@ import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {UsaOfficalComponent} from './usa-offical/usa-offical.component';
 import {LoginComponent} from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { ProfileComponent } from './profile/profile.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {ProfileComponent} from './profile/profile.component';
 import {Authentication} from './authentication';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {StepsComponent} from './steps/steps.component';
+import {PracticeComponent} from './practice/practice.component';
 
 const appRoutes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegistrationComponent},
     {path: 'profile', component: ProfileComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'steps', component: StepsComponent},
+    {path: 'practice', component: PracticeComponent},
     {path: '*', redirectTo: 'profile'}
 ];
 
@@ -27,6 +33,9 @@ const appRoutes: Routes = [
         LoginComponent,
         RegistrationComponent,
         ProfileComponent,
+        DashboardComponent,
+        StepsComponent,
+        PracticeComponent,
     ],
     imports: [
         BrowserModule,
