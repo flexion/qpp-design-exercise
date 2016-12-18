7988b1833881a5425e1812fbc9336050667892eb
Seth Alt <seth@sweepingdesign.com>
2016-12-18 12:22:18 -0600

    added angular attributes to profile form

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../user';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+
+  user = new User();
+
+  onSubmit() {
+
+  }
 
   constructor() { }
 
