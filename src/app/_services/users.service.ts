c893834f0ee171d51ff2f84a0c5731651d527525
Dylan Oliver <doliver@flexion.us>
2016-12-19 13:28:32 -0800

    npm / ts regulation

--- src/app/_services/users.service.ts
+++ src/app/_services/users.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {User} from '../_models/user';
 import {Registration} from '../_models/registration';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -9,16 +10,19 @@ export class UsersService {
     constructor(private http: Http) {
     }
 
-    getUsers() {
-        return this.http.get('/api/users');
+    getUsers(): Observable<User[]> {
+        return this.http.get('/api/users')
+            .map((r: Response) => r.json());
     }
 
     updateUser(user: User) {
-        return this.http.put(`/api/users/${user.id}`, JSON.stringify(user));
+        return this.http.put(`/api/users/${user.id}`, JSON.stringify(user))
+            .map((r: Response) => r.json());
     }
 
     registerUser(registration: Registration) {
-        return this.http.post('/api/users', registration);
+        return this.http.post('/api/users', registration)
+            .map((r: Response) => r.json());
     }
 
 
